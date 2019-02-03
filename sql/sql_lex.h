50799ce64fa120df8c29245038011565f8e6f910
Nikita Malyavin <nikitamalyavin@gmail.com>
2019-02-04 09:37:58 +1000

    MDEV-16973 Application-time periods: DELETE
    * inject portion of time updates into mysql_delete main loop
    * triggered case emits delete+insert, no updates
    * PORTION OF `SYSTEM_TIME` is forbidden
    * `DELETE HISTORY .. FOR PORTION OF ...` is forbidden as well

--- sql/sql_lex.h
+++ sql/sql_lex.h
@@ -1261,6 +1261,7 @@ public:
   /* push new Item_field into item_list */
   bool vers_push_field(THD *thd, TABLE_LIST *table, const LEX_CSTRING field_name);
 
+  Item* period_setup_conds(THD *thd, TABLE_LIST *table, Item *where);
   void init_query();
   void init_select();
   st_select_lex_unit* master_unit() { return (st_select_lex_unit*) master; }
@@ -3321,6 +3322,7 @@ public:
 
   /* System Versioning */
   vers_select_conds_t vers_conditions;
+  vers_select_conds_t period_conditions;
 
   inline void free_set_stmt_mem_root()
   {
